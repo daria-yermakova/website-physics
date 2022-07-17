3649236249aa13a2b4aaf02c67018ff809e8d1f2
daria-yermakova <daria.yerm0710@gmail.com>
2022-07-17 15:42:16 +0300

    [master]

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./features/components/Header/Header";
+import Aside from "./features/components/Aside/Aside";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <Aside />
+            <div>website for true physics</div>
+        </div>
+    );
 }
 
 export default App;
